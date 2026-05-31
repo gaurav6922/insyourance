abed2684b39e158fbf9c090776952038ee431a86
Gaurav Arora <62440771+gaurav6922@users.noreply.github.com>
2026-05-31 17:58:30 +0530

    css fail

--- resources/views/partials/site-fonts.blade.php
+++ resources/views/partials/site-fonts.blade.php
@@ -1,7 +1,7 @@
 <style id="site-font-faces">
 @font-face {
     font-family: "insul-icon";
-    src: url("{{ public_asset('fonts/icons/insul-icon-1.0.4.woff2') }}") format("woff2");
+    src: url("{{ asset('fonts/icons/insul-icon-1.0.4.woff2') }}") format("woff2");
     font-display: swap;
 }
 @font-face {
@@ -9,18 +9,18 @@
     font-style: normal;
     font-weight: 400;
     font-display: block;
-    src: url("{{ public_asset('fonts/font-awesome/fa-brands-400.woff2') }}") format("woff2");
+    src: url("{{ asset('fonts/font-awesome/fa-brands-400.woff2') }}") format("woff2");
 }
 @font-face {
     font-family: "Font Awesome 5 Free";
     font-style: normal;
     font-weight: 900;
     font-display: block;
-    src: url("{{ public_asset('fonts/font-awesome/fa-solid-900.woff2') }}") format("woff2");
+    src: url("{{ asset('fonts/font-awesome/fa-solid-900.woff2') }}") format("woff2");
 }
 @font-face {
     font-family: eicons;
-    src: url("{{ public_asset('fonts/eicons/eicons.woff2') }}?5.34.0") format("woff2");
+    src: url("{{ asset('fonts/eicons/eicons.woff2') }}?5.34.0") format("woff2");
     font-weight: 400;
     font-style: normal;
     font-display: swap;
